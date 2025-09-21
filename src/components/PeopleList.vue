a1fd832e504e7fc9df97d379840861a96ec99f6d
Komarov Daniil <komarow.daniil@gmail.com>
2025-09-21 21:26:34 +0300

    feat: PeopleList component

---
+++ src/components/PeopleList.vue
@@ -0,0 +1,61 @@
+<template>
+  <div
+    class="overflow-auto border rounded p-2"
+    :style="{ maxHeight: maxHeight + 'px' }"
+  >
+    <PersonCard
+      v-for="person in friends"
+      :key="'friend-' + person.getUserUID()"
+      :person="person"
+      :relation="RelationToAuthorizedUser.FRIEND"
+      :profile="profile"
+    />
+    <PersonCard
+      v-for="person in pending"
+      :key="'pending-' + person.getUserUID()"
+      :person="person"
+      :relation="RelationToAuthorizedUser.PENDING_RESPONSE_TO_REQUEST"
+      :profile="profile"
+    />
+    <PersonCard
+      v-for="person in noRelation"
+      :key="'no-rel-' + person.getUserUID()"
+      :person="person"
+      :relation="RelationToAuthorizedUser.NO_RELATION"
+      :profile="profile"
+    />
+    <PersonCard
+      v-for="person in sentRequests"
+      :key="'sent-' + person.getUserUID()"
+      :person="person"
+      :relation="RelationToAuthorizedUser.SENT_FRIEND_REQUEST"
+      :profile="profile"
+    />
+    <p v-if="isEmpty" class="text-muted">Нет пользователей</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Person } from "@/core/Person";
+import type { Profile } from "@/core/Profile";
+import { defineProps, computed } from "vue";
+import PersonCard from "./PersonCard.vue";
+import { RelationToAuthorizedUser } from "@/core/Enum";
+
+const props = defineProps<{
+  profile: Profile;
+  friends: Person[];
+  pending: Person[];
+  noRelation: Person[];
+  sentRequests: Person[];
+  maxHeight?: number; // px
+}>();
+
+const isEmpty = computed(
+  () =>
+    props.friends.length === 0 &&
+    props.pending.length === 0 &&
+    props.noRelation.length === 0 &&
+    props.sentRequests.length === 0
+);
+</script>
