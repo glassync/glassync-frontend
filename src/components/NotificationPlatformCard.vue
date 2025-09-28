f6b1fe72ac76b180e859f412dfed3464ed775c73
Komarov Daniil <komarow.daniil@gmail.com>
2025-09-28 05:08:21 +0300

    fix: NotificationPlatform style

--- src/components/NotificationPlatformCard.vue
+++ src/components/NotificationPlatformCard.vue
@@ -1,19 +1,22 @@
 <template>
-  <div class="card p-2 mb-2 d-flex align-items-center">
-    <input
-      type="checkbox"
-      class="form-check-input me-3"
-      :checked="platform.getIsActive()"
-      @change="toggleActive"
-    />
-    <span class="flex-grow-1">{{ platform.getPlatformName() }}</span>
-    <a
-      :href="platform.getIdentifier()"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      {{ platform.getIdentifier() }}
-    </a>
+  <div class="card p-2 mb-2">
+    <div class="d-flex align-items-center gap-3">
+      <input
+        type="checkbox"
+        class="form-check-input"
+        :checked="platform.getIsActive()"
+        @change="toggleActive"
+      />
+      <span class="flex-shrink-0">{{ platform.getPlatformName() }}:</span>
+      <a
+        :href="platform.getIdentifier()"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="text-break"
+      >
+        {{ platform.getIdentifier() }}
+      </a>
+    </div>
   </div>
 </template>
 
@@ -28,7 +31,6 @@ const props = defineProps<{
 function toggleActive(event: Event) {
   const checkbox = event.target as HTMLInputElement;
   props.platform.setIsActive(checkbox.checked);
-  // Здесь можно добавить логику сохранения изменений (может, запрос к серверу?)
 }
 </script>
 
