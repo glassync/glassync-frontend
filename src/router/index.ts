506c11248138f64bba09890e9326e03efe09aa91
Komarov Daniil <komarow.daniil@gmail.com>
2025-11-20 23:52:10 +0300

    test: for Ilya 2

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,13 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Calendar from "../components/Calendar.vue";
-import FriendsPage from "../views/FriendView.vue";
-import UserProfile from "../views/AccountView.vue";
+import FriendsPage from "../views/PeopleView.vue";
+import UserProfile from "../views/ProfileView.vue";
 import GlassiesPage from "../views/GlassiesView.vue";
-import AuthorizationView from "../views/AuthorizationView.vue";
-import RegistrationView from "../views/RegistrationView.vue";
-import EditProfileView from "../views/EditProfileView.vue";
-import EventView from "../views/EventView.vue";
-import MainView from "../views/MainView.vue";
-import PeopleView from "../views/PeopleView.vue";
-import ProfileView from "../views/ProfileView.vue";
 
+// Явная типизация маршрутов
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "Main",
-    component: MainView,
-  },
-  {
-    path: "/calendar",
     name: "Calendar",
     component: Calendar,
   },
@@ -36,38 +25,6 @@ const routes: Array<RouteRecordRaw> = [
     path: "/glassies",
     name: "Glassies",
     component: GlassiesPage,
-  },
-  {
-    path: "/login",
-    name: "Authorization",
-    component: AuthorizationView,
-  },
-  {
-    path: "/register",
-    name: "Registration",
-    component: RegistrationView,
-  },
-  {
-    path: "/edit-profile",
-    name: "EditProfile",
-    component: EditProfileView,
-  },
-  {
-    path: "/event/:id",
-    name: "Event",
-    component: EventView,
-    props: true,
-  },
-  {
-    path: "/people",
-    name: "People",
-    component: PeopleView,
-  },
-  {
-    path: "/profile/:id",
-    name: "Profile",
-    component: ProfileView,
-    props: true,
   },
 ];
 
