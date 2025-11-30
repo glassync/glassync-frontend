00c949c6cb0098bb1f7bf1f8c7cf76903ff22b3e
Komarov Daniil <komarow.daniil@gmail.com>
2025-11-30 03:49:37 +0300

    fix: redirects to EventView

--- src/views/EventView.vue
+++ src/views/EventView.vue
@@ -7,12 +7,30 @@
 <script setup lang="ts">
 import type { Profile } from "@/core/Profile";
 import type { Event } from "@/core/Event";
-import { defineProps } from "vue";
-
+import { ref, watch, defineProps } from "vue";
 import EventForm from "@/components/EventForm.vue";
+import { Events } from "@/core/Events";
 
-defineProps<{
+const props = defineProps<{
   profile: Profile;
-  event?: Event;
+  eventUID?: number | string;
 }>();
+
+const event = ref<Event | null>(null);
+
+const events = Events.getInstance();
+
+watch(
+  () => props.eventUID,
+  (uid) => {
+    if (uid !== undefined) {
+      const numericUID = Number(uid);
+      const findEvent = events.getEvent(numericUID);
+      event.value = findEvent ?? null;
+    } else {
+      event.value = null;
+    }
+  },
+  { immediate: true }
+);
 </script>
